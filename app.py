aff108d9ac414b2c37590328c980545aa554e3a7
runalp97 <as4922011@gmail.com>
2025-03-03 14:24:34 +0000

    Updated app.py  with new changes2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, request, jsonify
 import os
 
@@ -6,7 +7,7 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def homepage():
     if request.method == "GET":
-        return jsonify({"message": "now that the pipeline has been made the 2nd developer wants some schnage in code this is the new code pipeline will again start!"})
+        return jsonify({"message": "now that the pipeline has been made the 2nd developer wants some change in code this is the new code pipeline will again start!"})
 
 PORT = int(os.environ.get("PORT", 8080))
 if __name__ == '__main__':
